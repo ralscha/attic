54b1726e588befcb34624d136be4a0094e8e1710
Ralph Schaer <ralphschaer@gmail.com>
2026-03-17 17:20:02 +0100

    retire old examples

---
+++ geotracker/client/src/app/app-position.ts
@@ -0,0 +1,8 @@
+export interface AppPosition {
+  accuracy: number;
+  bearing: number | null;
+  latitude: number;
+  longitude: number;
+  speed: number | null;
+  time: number;
+}
---
+++ geotracker/client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  serverURL: 'https://your-public-server.example.com',
+};
